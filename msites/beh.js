eece3a4568c71c3b7eb9f35190e7ea5be48dfadd
sweatysoybean <1285314876@qq.com>
2023-05-24 21:30:05 +0800

    2023/5/24 21:30

---
+++ msites/beh.js
@@ -0,0 +1,9 @@
+var openingAnimObj=document.getElementById("introd");
+var oaoTitle=document.getElementById("introdtitle");
+var oaoText=document.getElementsByClassName("introdparag");
+openingAnimObj.style.paddingLeft="64px";
+oaoTitle.style.color="#fff";
+
+for (var i of oaoText){
+    i.style.color="#fff";
+}
