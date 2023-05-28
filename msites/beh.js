4cb3ce75e198c183900725512f3a141e33c5d664
sweatysoybean <1285314876@qq.com>
2023-05-28 23:02:51 +0800

    2023/5/28 23:02

--- msites/beh.js
+++ msites/beh.js
@@ -1,7 +1,10 @@
 var openingAnimObj=document.getElementById("introd");
 var oaoTitle=document.getElementById("introdtitle");
 var oaoText=document.getElementsByClassName("introdparag");
-openingAnimObj.style.paddingLeft="64px";
+var oaoPld=Number(window.getComputedStyle(openingAnimObj,null)["padding-left"].slice(0,-2));
+oaoPld+=32;
+openingAnimObj.style.paddingLeft=oaoPld.toString()+"px";
+
 oaoTitle.style.color="#fff";
 
 for (var i of oaoText){
